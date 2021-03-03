717d67cdb7e23a8866afc206b6bff40433ce0f71
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-03-03 17:05:58 +0530

    BookingClasses done

--- BookingClasses.cpp
+++ BookingClasses.cpp
@@ -1,1 +1,117 @@
 #include "BookingClasses.h"
+
+//class BookingClasses
+
+BookingClasses::BookingClasses(const string &name, const bool &isAC, const bool &IsLuxury, const double &loadFactor) : name_(name), isAC_(isAC), isLuxury_(IsLuxury), loadFactor_(loadFactor) {}
+BookingClasses::~BookingClasses() {}
+
+//class SittingClasses
+
+SittingClasses::SittingClasses(const string &name, const bool &isAC, const bool &IsLuxury, const double &loadFactor) : BookingClasses(name, isAC, IsLuxury, loadFactor) {}
+SittingClasses::~SittingClasses() {}
+
+//class SleepingClasses
+
+SleepingClasses::SleepingClasses(const string &name, const bool &isAC, const bool &IsLuxury, const double &loadFactor) : BookingClasses(name, isAC, IsLuxury, loadFactor) {}
+SleepingClasses::~SleepingClasses() {}
+
+//class Sleeping2Tier
+
+Sleeping2Tier::Sleeping2Tier(const string &name, const bool &isAC, const bool &IsLuxury, const double &loadFactor) : SleepingClasses(name, isAC, IsLuxury, loadFactor) {}
+Sleeping2Tier::~Sleeping2Tier() {}
+
+//class Sleeping3Tier
+
+Sleeping3Tier::Sleeping3Tier(const string &name, const bool &isAC, const bool &IsLuxury, const double &loadFactor) : SleepingClasses(name, isAC, IsLuxury, loadFactor) {}
+Sleeping3Tier::~Sleeping3Tier() {}
+
+//class ACFirstClass
+
+ACFirstClass::ACFirstClass() : Sleeping2Tier(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+ACFirstClass::~ACFirstClass() {}
+
+const string ACFirstClass::sName = "AC First Class";
+const bool ACFirstClass::sIsAC = true;
+
+const ACFirstClass &ACFirstClass::Type(){
+    static const ACFirstClass sACFirstClass;
+    return sACFirstClass;
+}
+
+//class AC2Tier
+
+AC2Tier::AC2Tier() : Sleeping2Tier(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+AC2Tier::~AC2Tier() {}
+
+const string AC2Tier::sName = "AC 2 Tier";
+const bool AC2Tier::sIsAC = true;
+
+const AC2Tier &AC2Tier::Type(){
+    static const AC2Tier sAC2Tier;
+    return sAC2Tier;
+}
+
+//class FirstClass
+
+FirstClass::FirstClass() : Sleeping2Tier(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+FirstClass::~FirstClass() {}
+
+const string FirstClass::sName = "First Class";
+const bool FirstClass::sIsAC = false;
+
+const FirstClass &FirstClass::Type(){
+    static const FirstClass sFirstClass;
+    return sFirstClass;
+}
+
+//class AC3Tier
+
+AC3Tier::AC3Tier() : Sleeping3Tier(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+AC3Tier::~AC3Tier() {}
+
+const string AC3Tier::sName = "AC 3 Tier";
+const bool AC3Tier::sIsAC = true;
+
+const AC3Tier &AC3Tier::Type(){
+    static const AC3Tier sAC3Tier;
+    return sAC3Tier;
+}
+
+//class ACChairCar
+
+ACChairCar::ACChairCar() : SittingClasses(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+ACChairCar::~ACChairCar() {}
+
+const string ACChairCar::sName = "AC Chair Car";
+const bool ACChairCar::sIsAC = true;
+
+const ACChairCar &ACChairCar::Type(){
+    static const ACChairCar sACChairCar;
+    return sACChairCar;
+}
+
+//class Sleeper
+
+Sleeper::Sleeper() : Sleeping3Tier(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+Sleeper::~Sleeper() {}
+
+const string Sleeper::sName = "Sleeper";
+const bool Sleeper::sIsAC = false;
+
+const Sleeper &Sleeper::Type(){
+    static const Sleeper sSleeper;
+    return sSleeper;
+}
+
+//class SecondSitting
+
+SecondSitting::SecondSitting() : SittingClasses(sName, sIsAC, sIsLuxury, sLoadFactor) {}
+SecondSitting::~SecondSitting() {}
+
+const string SecondSitting::sName = "Second Sitting";
+const bool SecondSitting::sIsAC = false;
+
+const SecondSitting &SecondSitting::Type(){
+    static const SecondSitting sSecondSitting;
+    return sSecondSitting;
+}
--- BookingClasses.h
+++ BookingClasses.h
@@ -1,69 +1,185 @@
-#ifndef BOOKINGCLASSES_HPP
-#define BOOKINGCLASSES_HPP
+#ifndef _BOOKINGCLASSES_H
+#define _BOOKINGCLASSES_H
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 class BookingClasses{
+    
+    private:
+        const string &name_;
+        const bool &isAC_, &isLuxury_;
+        const double &loadFactor_;
 
-    public:
-        BookingClasses();
+    protected:
+        BookingClasses(const string &, const bool &, const bool &, const double &);
         virtual ~BookingClasses();
 
-        virtual double GetLoadFactor() const = 0;
-        virtual string GetName() const = 0;
+    public:
+        inline string GetName() const{
+            return name_;
+        }
+        inline bool IsAC() const{
+            return isAC_;
+        }
+        inline bool IsLuxury() const{
+            return isLuxury_;
+        }
+        inline double GetLoadFactor() const{
+            return loadFactor_;
+        }
         virtual bool IsSitting() const = 0;
-        virtual bool IsAC() const = 0;
         virtual uint32_t GetNumberOfTiers() const = 0;
-        virtual bool IsLuxury() const = 0;
 };
 
 class SittingClasses : public BookingClasses{
 
-    public:
-        SittingClasses();
+    protected:
+        SittingClasses(const string &, const bool &, const bool &, const double &);
         ~SittingClasses();
 
+    public:
         inline bool IsSitting() const{
             return true;
         }
         inline uint32_t GetNumberOfTiers() const{
-            return 0;
+            return 0U;
         }
 };
 
 class SleepingClasses : public BookingClasses{
-
-    public:
-        SleepingClasses();
+    
+    protected:
+        SleepingClasses(const string &, const bool &, const bool &, const double &);
         ~SleepingClasses();
 
-        inline bool isSitting() const{
+    public:
+        inline bool IsSitting() const{
             return false;
         }
 };
 
 class Sleeping2Tier : public SleepingClasses{
-
-    public:
-        Sleeping2Tier();
+    
+    protected:
+        Sleeping2Tier(const string &, const bool &, const bool &, const double &);
         ~Sleeping2Tier();
 
+    public:
         inline uint32_t GetNumberOfTiers() const{
-            return 2;
+            return 2U;
         }
 };
 
 class Sleeping3Tier : public SleepingClasses{
 
-    public:
-        Sleeping3Tier();
+    protected:
+        Sleeping3Tier(const string &, const bool &, const bool &, const double &);
         ~Sleeping3Tier();
 
+    public:
         inline uint32_t GetNumberOfTiers() const{
-            return 3;
+            return 3U;
         }
+};
+
+class ACFirstClass : public Sleeping2Tier{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        ACFirstClass();
+        ~ACFirstClass();
+    
+    public:
+        static const ACFirstClass &Type();
+};
+
+class AC2Tier : public Sleeping2Tier{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        AC2Tier();
+        ~AC2Tier();
+    
+    public:
+        static const AC2Tier &Type();
+};
+
+class FirstClass : public Sleeping2Tier{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        FirstClass();
+        ~FirstClass();
+    
+    public:
+        static const FirstClass &Type();
+};
+
+class AC3Tier : public Sleeping3Tier{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        AC3Tier();
+        ~AC3Tier();
+    
+    public:
+        static const AC3Tier &Type();
+};
+
+class ACChairCar : public SittingClasses{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        ACChairCar();
+        ~ACChairCar();
+    
+    public:
+        static const ACChairCar &Type();
+};
+
+class Sleeper : public Sleeping3Tier{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        Sleeper();
+        ~Sleeper();
+    
+    public:
+        static const Sleeper &Type();
+};
+
+class SecondSitting : public SittingClasses{
+
+    private:
+        static const string sName;
+        static const bool sIsAC, sIsLuxury;
+        static const double sLoadFactor;
+
+        SecondSitting();
+        ~SecondSitting();
+    
+    public:
+        static const SecondSitting &Type();
 };
 
 #endif
