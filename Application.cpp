b993ae772eb60b3a0e51188816a2ecb1f7e9f099
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-03-03 17:17:02 +0530

    Application added

--- Application.cpp
+++ Application.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello World!"<<endl;
-    return 0;
+#include "BookingClasses.h"
+#include "Booking.h"
+
+const bool ACFirstClass::sIsLuxury = true;
+const double ACFirstClass::sLoadFactor = 3.00;
+
+const bool AC2Tier::sIsLuxury = false;
+const double AC2Tier::sLoadFactor = 2.00;
+
+const bool FirstClass::sIsLuxury = true;
+const double FirstClass::sLoadFactor = 2.00;
+
+const bool AC3Tier::sIsLuxury = false;
+const double AC3Tier::sLoadFactor = 1.75;
+
+const bool ACChairCar::sIsLuxury = false;
+const double ACChairCar::sLoadFactor = 1.25;
+
+const bool Sleeper::sIsLuxury = false;
+const double Sleeper::sLoadFactor = 1.00;
+
+const bool SecondSitting::sIsLuxury = false;
+const double SecondSitting::sLoadFactor = 0.50;
+
+void debug(const BookingClasses &obj){
+    cout<<obj<<endl;
 }
