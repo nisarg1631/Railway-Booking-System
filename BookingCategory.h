ea625a58e08eb23d13af468900c49861adf9ba25
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-04-08 02:23:25 +0530

    major part done

--- BookingCategory.h
+++ BookingCategory.h
@@ -4,4 +4,21 @@
 #ifndef _BOOKINGCATEGORY_H
 #define _BOOKINGCATEGORY_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Passenger; // forward declaration
+
+// generic BookingCategory type to generate different bookings
+template<typename T>
+class BookingCategoryTypes;
+
+class BookingCategory{
+    private:
+        const string name_;
+    
+};
+
 #endif
